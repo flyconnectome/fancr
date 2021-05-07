5f850b2143c60bd56fcecf3ac25e227aea0f2343
Gregory Jefferis <jefferis@gmail.com>
2021-05-07 17:00:47 +0100

    test for xyz to supervoxel mapping

--- tests/testthat/test-ids.R
+++ tests/testthat/test-ids.R
@@ -1,4 +1,8 @@
 test_that("fanc_xyz2id works", {
   expect_equal(fanc_xyz2id(cbind(34495, 82783, 1954), rawcoords=TRUE),
                "648518346479013777")
+
+  expect_equal(
+    fanc_xyz2id(cbind(34495, 82783, 1954), rawcoords=TRUE, root=F),
+    "73186243730767724")
 })
