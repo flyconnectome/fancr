9324c5bbd909b75b03dec828d7e0efb37f5c3210
Gregory Jefferis <jefferis@gmail.com>
2021-05-27 13:53:38 +0100

    Fast fanc xyz2id mpping
    * needs latest cloudvolume

--- R/ids.R
+++ R/ids.R
@@ -42,9 +42,9 @@ fanc_leaves <- function(x, integer64=TRUE, ...) {
 }
 
 
-#' Convert xyz location to a root or supervoxel id
+#' Convert xyz locations to root or supervoxel ids
 #'
-#' @details This is currently very slow because we do not have a supervoxel
+#' @details This used to be very slow because we do not have a supervoxel
 #'   field on spine.
 #'
 #'   I am somewhat puzzled by the voxel dimensions for FANC. Neuroglancer
@@ -62,12 +62,45 @@ fanc_leaves <- function(x, integer64=TRUE, ...) {
 #' @examples
 #' # a point from neuroglancer, should map to 648518346479013777
 #' fanc_xyz2id(cbind(34495, 82783, 1954), rawcoords=TRUE)
-fanc_xyz2id <- function(xyz, rawcoords=FALSE, voxdims=c(4.25, 4.25, 45),
+fanc_xyz2id <- function(xyz, rawcoords=FALSE, voxdims=c(4.3, 4.3, 45),
                         root=TRUE, ...){
-  ids=flywire_xyz2id(xyz, rawcoords = rawcoords, voxdims = voxdims,
-                     cloudvolume.url = fanc_cloudvolume_url(),
-                     root=root, ...,
-                     method = 'cloudvolume')
-  ids
+  if(rawcoords)
+    xyz=scale(xyzmatrix(xyz), center = F, scale = 1/voxdims)
+  svids=fanc_supervoxels(xyz, voxdims=voxdims)
+  if(isTRUE(root)) fanc_rootid(svids) else svids
+}
+
+#' rawxyz=cbind(34496, 82782, 1954)
+#' nmxyz=cbind(34496, 82782, 1954)*c(4.3,4.3,45)
+fanc_supervoxels <- function(x, voxdims=c(4.3,4.3,45)) {
+  pts=scale(xyzmatrix(x), center = F, scale = voxdims)
+  nas=rowSums(is.na(pts))>0
+  if(any(nas)) {
+    svids=rep("0", nrow(pts))
+    svids[!nas]=fanc_supervoxels(pts[!nas,,drop=F], voxdims = c(1,1,1))
+    return(svids)
+  }
+  u="https://services.itanna.io/app/transform-service/query/dataset/fanc_v4/s/2/values_array_string_response"
+  body=jsonlite::toJSON(list(x=pts[,1], y=pts[,2], z=pts[,3]))
+  res=httr::POST(u, body = body)
+  httr::stop_for_status(res)
+  j=httr::content(res, as='text', encoding = 'UTF-8')
+  svids=unlist(jsonlite::fromJSON(j, simplifyVector = T), use.names = F)
+  svids
+}
+
+
+fanc_supervoxels_binary <- function(x, voxdims=c(4,4,40)) {
+  pts=scale(xyzmatrix(x), center = F, scale = voxdims)
+  ptsb=writeBin(as.vector(pts), con = raw(), size=4)
+ y
+  u="https://spine.janelia.org/app/transform-service/query/dataset/flywire_190410/s/2/values_binary/format/array_float_3xN"
+
+  res=httr::POST(u, body=ptsb, encode = "raw")
+  httr::stop_for_status(res)
+  arr=httr::content(res)
+  bytes=readBin(arr, what = numeric(), n=length(arr)/8, size = 8, endian = 'little')
+  class(bytes)="integer64"
+  bit64::as.character.integer64(bytes)
 }
 
