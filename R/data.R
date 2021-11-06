9262567d38181f00ed95026564b641627f45b1ab
Dominik Krzeminski <raymon92@gmail.com>
2021-11-06 23:48:30 +0000

    surf mesh data description added

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Simplified tissue surface of FANC
+#'
+#' @name FANC.surf
+#' @docType data
+#' @description This is unsymmetrical and not normalized version of the mesh.
+#' @export
+#' @examples
+#' \dontrun{
+#' # Depends on nat
+#' library(nat)
+#' wire3d(FANC.surf)
+#' }
+"FANC.surf"
