e7cbd2d43ea7a3e7d4a6c63f8d8dfd401049ec8d
Gregory Jefferis <jefferis@gmail.com>
2022-02-23 23:23:51 +0000

    Add package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,10 @@
 
   # make FANC4<->FANC3 bridging registrations available
   register_fanc3to4()
+  packageStartupMessage(
+    'Do `choose_fanc()` to use many fafbseg::flywire_* functions!\n',
+    'Use dr_fanc() to get a report on your installation.\n',
+    'Trouble? Visit https://flyconnectome.github.io/fancr/SUPPORT.html or #code on FANC Slack')
 
   invisible()
 }
