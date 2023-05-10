e55ba939ea599de7d075245581676fed92c250bd
Gregory Jefferis <jefferis@gmail.com>
2023-05-10 19:07:47 +0100

    update ids in examples
    * they must be up to date so use fanc_latestid

--- R/partners.R
+++ R/partners.R
@@ -2,6 +2,9 @@
 #'
 #' @details note that the rootids you pass in must be up to date. See example.
 #'
+#' @param rootids Character vector specifying one or more flywire rootids. As a
+#'   convenience this argument is passed to \code{\link{fanc_ids}} allowing you
+#'   to pass in data.frames, flywire URLs or simple ids.
 #' @inheritParams fafbseg::flywire_partner_summary
 #' @param datastack_name An optional CAVE \code{datastack_name}. If unset a
 #'   sensible default is chosen.
@@ -11,19 +14,21 @@
 #' @export
 #'
 #' @examples
-#' head(fanc_partner_summary("648518346494405175"))
-#' head(fanc_partner_summary("648518346494405175", partners='inputs'))
+#' # NB id must be up to date
+#' sample_id=fanc_latestid("648518346481082458")
+#' head(fanc_partner_summary(sample_id))
+#' head(fanc_partner_summary(sample_id, partners='inputs'))
 #' \dontrun{
 #' # get the latest id for an outdate
-#' fanc_partner_summary(fanc_latestid("648518346473954669"))
+#' fanc_partner_summary(("648518346473954669"))
 #'
 #' ## open fanc/flywire scene containing top partners
 #' library(dplyr)
-#' fanc_partner_summary("648518346494405175", partners='inputs') %>%
+#' fanc_partner_summary(fanc_latestid("648518346494405175"), partners='inputs') %>%
 #'   slice_max(weight, n = 20) %>%
 #'   fanc_scene(open=TRUE)
 #' }
-fanc_partner_summary <- function(rootid, partners = c("outputs", "inputs"),
+fanc_partner_summary <- function(rootids, partners = c("outputs", "inputs"),
                                  threshold = 0,
                                  remove_autapses = TRUE,
                                  cleft.threshold = 0,
@@ -33,7 +38,7 @@ fanc_partner_summary <- function(rootid, partners = c("outputs", "inputs"),
     datastack_name = fanc_datastack_name()
   with_fanc(
     fafbseg::flywire_partner_summary(
-      rootid,
+      rootids,
       threshold = threshold,
       partners=partners,
       method = "cave",
