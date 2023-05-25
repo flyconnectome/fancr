76b4922a322f2b81a80c297d6be7a73315815dec
Gregory Jefferis <jefferis@gmail.com>
2023-05-25 12:55:11 +0100

    add L2 skeletons/dotprops

---
+++ R/l2.R
@@ -0,0 +1,62 @@
+#' Read L2 skeleton or dotprops for FANC neurons using fafbseg-py
+#'
+#' @description \code{fanc_read_l2skel} reads one or more neurons as simplified
+#'   L2 skeletons.
+#'
+#' @description \code{fanc_read_l2dp} reads one or more neurons as simplified
+#'   dotprops format. See \code{\link[fafbseg]{read_l2skel}}.
+#' @param dataset An optional CAVE dataset name (expert use only, by default
+#'   will choose the standard FANC dataset).
+#' @inheritParams fafbseg::read_l2skel
+#'
+#' @details \code{fanc_read_l2dp} uses a special data structure for rapid
+#'   download of the dotprops version of neurons required for NBLASTing. It
+#'   leverages the python navis / fafbseg-py packages and you will need to
+#'   install these, typically using the \code{\link[fafbseg]{simple_python}}
+#'   function.
+#'
+#'   See \code{\link[fafbseg]{read_l2skel}} for additional details.
+#'
+#' @return a \code{\link{neuronlist}} containing one or more
+#'   \code{\link{neuron}} or \code{\link{dotprops}} objects. Note that neurons
+#'   will be calibrated in nm while dotprops will be calibrated in microns.
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' # one time install of necessary python packages
+#' fafbseg::simple_python(pkgs="fafbseg")
+#'
+#' dnp42=c("648518346507131167", "648518346485772414")
+#' dnp42.latest=fanc_latestid(dnp42)
+#' dnp42.dps <- fanc_read_l2dp(dnp42.latest)
+#'
+#' # plot those
+#' nclear3d()
+#' plot3d(dnp42.dps, lwd=3)
+#' # nb dotprops are always in microns
+#' wire3d(FANC.surf/1e3, col='grey')
+#'
+#' nclear3d()
+#' dnp42.skel <- fanc_read_l2skel(dnp42.latest)
+#' plot3d(dnp42.skel, lwd=2)
+#' # nb neuron skeletons are in nm
+#' wire3d(FANC.surf, col='grey')
+#' }
+fanc_read_l2dp <- function(id, OmitFailures=TRUE, dataset=NULL, ...) {
+  id=fanc_ids(id)
+  if(is.null(dataset))
+    dataset=with_fanc(getOption("fafbseg.cave.datastack_name"))
+  fafbseg::read_l2dp(id, dataset=dataset, OmitFailures=OmitFailures, ...)
+}
+
+#' @export
+#' @rdname fanc_read_l2dp
+fanc_read_l2skel <- function(id, OmitFailures=TRUE, dataset=NULL, ...) {
+  id=fanc_ids(id)
+  if(is.null(dataset))
+    dataset=with_fanc(getOption("fafbseg.cave.datastack_name"))
+  fafbseg::read_l2skel(id, dataset=dataset, OmitFailures=OmitFailures, ...)
+}
+
