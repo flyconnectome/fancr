6d756eab7bcbe736b172bc51635d31c15024753f
Gregory Jefferis <jefferis@gmail.com>
2023-09-16 18:19:05 +0100

    switch package startup message to .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,12 @@
 
   # make FANC4<->FANC3 bridging registrations available
   register_fanc3to4()
+  invisible()
+}
+
+.onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     'Do `choose_fanc()` to use many fafbseg::flywire_* functions!\n',
     'Use dr_fanc() to get a report on your installation.\n',
     'Trouble? Visit https://flyconnectome.github.io/fancr/SUPPORT.html or #code on FANC Slack')
-
-  invisible()
 }
